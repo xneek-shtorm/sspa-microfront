cc1a46bf8bcf513399cfb52b1545ee398e446e9c
Nikolay Fedoseev <n.fedoseev@whyhappen.ru>
2025-08-01 10:44:22 +0300

    app2 with routing

---
+++ packages/app-2/src/root.component.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@xneek/ui-kit";
+
+export default function Root(props) {
+  return <section style={{ display: "flex", flexDirection: 'column', gap: 16 }}>
+    APP-2 MICROFRONTEND CONTENT 2️⃣
+  </section>;
+}
